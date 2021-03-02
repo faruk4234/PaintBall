ce3badcfdef7572502a7cc07132af4aeed9f2014
faruk4234 <60069276+faruk4234@users.noreply.github.com>
2021-03-02 22:52:28 +0300

    ok colors

--- mobile/src/components/Input.js
+++ mobile/src/components/Input.js
@@ -2,15 +2,11 @@ import * as React from 'react'
 
 import { TextInput } from 'react-native-paper'
 
-const Input = ({ name }) => {
-    const [text, setText] = React.useState('')
-
-    return (
-        <TextInput
-            label={JSON.stringify(name)}
-            value={text}
-            onChangeText={(text) => setText(text)} />
-    )
-}
+const Input = ({ name, text, setText }) => (
+    <TextInput
+        label={JSON.stringify(name)}
+        value={text}
+        onChangeText={(text) => setText(text)} />
+)
 
 export default Input
--- mobile/src/screen/LoginScreen/LoginScreen.js
+++ mobile/src/screen/LoginScreen/LoginScreen.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import { View, StyleSheet } from 'react-native'
+
 import Input from '../../components/Input'
 
+const [text, setText] = React.useState('')
+
 const LoginScreen = () => {
-    <Input name='Your Name' />
+    <View style={styles.container}>
+        <Input
+            name='Your Name'
+            text={text}
+            setText={setText} />
+    </View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#76D7C4'
+    }
+})
 
 export default LoginScreen
